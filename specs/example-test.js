88a24aa1eda23f423d821a3812f754655e24e245
Robert DeLuca <robertdeluca19@gmail.com>
2021-03-03 14:02:18 -0600

    ♻️Update example todomvc app

--- specs/example-test.js
+++ specs/example-test.js
@@ -1,27 +1,27 @@
-const assert = require('assert')
-const httpServer = require('http-server')
-const { percySnapshot } = require('@percy/webdriverio');
+const assert = require('assert');
+const httpServer = require('http-server');
+const percySnapshot = require('@percy/webdriverio');
 
-describe('example page', function() {
-  const PORT = 8000
-  const TEST_URL = `http://localhost:${PORT}`
+describe('example page', function () {
+  const PORT = 8000;
+  const TEST_URL = `http://localhost:${PORT}`;
 
-  let server = null
+  let server = null;
 
-  before(function() {
+  before(function () {
     // Start local server to host app under test.
-    server = httpServer.createServer({ root: `${__dirname}/../site` })
-    server.listen(PORT)
-  })
+    server = httpServer.createServer({ root: `${__dirname}/../` });
+    server.listen(PORT);
+  });
 
-  after(function() {
+  after(function () {
     // Shut down the HTTP server.
-    server.close()
-  })
+    server.close();
+  });
 
-  it('should look nice', async function() {
-    await browser.url(TEST_URL)
-    await percySnapshot(browser, 'sample')
-    assert.equal(await browser.getTitle(), 'jQuery • TodoMVC')
-  })
-})
+  it('should look nice', async function () {
+    await browser.url(TEST_URL);
+    await percySnapshot(browser, 'sample');
+    assert.equal(await browser.getTitle(), 'VanillaJS • TodoMVC');
+  });
+});
