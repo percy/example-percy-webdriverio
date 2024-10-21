f47ea73a2ad930159fa2b3119c4ae05481350006
Ninad Sheth <ninad@browserstack.com>
2024-10-21 17:18:19 +0530

    Set browser to headless

--- wdio.conf.js
+++ wdio.conf.js
@@ -51,7 +51,11 @@ exports.config = {
     //
     capabilities: [{
         // capabilities for local browser web tests
-        browserName: 'chrome' // or "firefox", "microsoftedge", "safari"
+        browserName: 'chrome', // or "firefox", "microsoftedge", "safari"
+        'goog:chromeOptions': {
+        args: ['headless', 'disable-gpu']
+    }
+
     }],
 
     //
